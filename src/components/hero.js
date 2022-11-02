eaae82202955674437c675e480a8ed5928d4e1d8
Guneet <guneet.iam@gmail.com>
2022-11-02 21:54:35 +0530

    css added

---
+++ src/components/hero.js
@@ -0,0 +1,14 @@
+export default function hero(){
+    return (
+        <div id="hero">
+        
+        <img id="heroImg"src=" " alt=" img"/>
+        <div id="heroText">   
+            <h1>Online Experiences</h1>
+            <p>Note that the development build is not optimized.
+To create a production build, use npm run build</p>
+        </div>
+
+        </div>
+    )
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import logo from "../img/airbnb.png"
+export default function navbar() {
+    return (
+        <nav>
+            <img id="logo" alt="img" src={logo}/>
+        <h1>airbnb</h1>
+        </nav>
+    )
+};
